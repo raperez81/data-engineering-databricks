7414949d7558bb86be98c6e2b7af5845d0cf78c3
Ricardo Perez <ricardo.perez@brooklyndata.co>
2025-10-17 09:56:06 -0600

    Create ingesting-data-into-delta-lake.sql

---
+++ ingesting-data-into-delta-lake.sql
@@ -0,0 +1,40 @@
+-- Change the default catalog/schema
+USE CATALOG dbacademy;
+USE SCHEMA IDENTIFIER(DA.schema_name);
+
+-- 1. Create table (CTAS)
+DROP TABLE IF EXISTS current_employees_ctas;
+
+CREATE TABLE current_employees_ctas
+AS
+SELECT ID, FirstName, Country, Role
+FROM read_files(
+  'Volumes/dbacademy/' || DA.schema_name || '/myfiles/',
+  format => 'csv',
+  header => true,
+  inferSchema => true
+);
+
+-- 2. Upload UI
+
+-- 3. Copy into
+DROP TABLE IF EXISTS current_employees_copyinto;
+
+CREATE TABLE current_employees_copyinto (
+  ID INT,
+  FirstName STRING,
+  Country STRING,
+  Role STRING
+);
+
+spark.sql(f'''
+COPY INTO current_employees_copyinto
+  FROM 'Volumes/dbacademy/{DA.schema_name}/myfiles/'
+  FILEFORMAT = CSV
+  FORMAT_OPTIONS (
+    'header' = 'true',
+    'inferSchema' = 'true'
+  )
+  ''').display()
+
+DESCRIBE HISTORY current_employees_copyinto;
